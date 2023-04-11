1116c4ad4e417f866dd283f74699fb6153ff5c58
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-11 15:15:36 +0800

    created formrow for login

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Wrapper from '../assets/wrappers/Testing'
+import Wrapper from '../assets/wrappers/ErrorPage'
 import img from '../assets/images/not-found.svg'
 import { Link } from 'react-router-dom'
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import Wrapper from '../assets/wrappers/Testing';
+import { Logo } from '../components';
+import { FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
   name: '',
@@ -12,9 +14,48 @@ const initialState = {
 const Register = () => {
 
   const [values, setValues] = useState(initialState);
+
+  // global state and useNavigate
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  }
+
   return (
     <Wrapper className='full-page'>
-      register
+      <form className='form' onSubmit={handleSubmit}>
+        <Logo/>
+        <h3>Login</h3>
+
+        {/* name input */}
+        <FormRow 
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+
+        {/* email input */}
+        <FormRow 
+          type='text'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        
+        {/* password input */}
+        <FormRow 
+            type='text'
+            name='password'
+            value={values.password}
+            handleChange={handleChange}
+          />
+        <button type='submit' className='btn btn-block'>Submit</button>
+      </form>
     </Wrapper>
   )
 }
