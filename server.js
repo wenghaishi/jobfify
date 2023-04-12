543b11a21b286848833d6cfe7977a43bfda4e720
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-12 18:46:16 +0800

    setup middleware to handle error and not found scenarios

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express';
-
 const app = express();
+import notFoundMiddleware from './middleware/not-Found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
 
 app.get('/', (req, res) => {
+  throw new Error('error')
   res.send('welcome!')
 })
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
