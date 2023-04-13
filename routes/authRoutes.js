b22d6b8a8f7cdae7b8e13c9f7cc7e78bf9fa0667
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-13 10:50:40 +0800

    created authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import express from "express";
 const router = express.Router();
 
 import { login, register, updateUser } from "../controllers/authController.js";
@@ -6,3 +6,5 @@ import { login, register, updateUser } from "../controllers/authController.js";
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/updateUser").post(updateUser);
+
+export default router
--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import notFoundMiddleware from './middleware/not-Found.js';
-import errorHandlerMiddleware from './middleware/error-handler.js';
-import dotenv from 'dotenv';
+import notFoundMiddleware from "./middleware/not-Found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import dotenv from "dotenv";
 dotenv.config();
 
-import connectDB from './db/connect.js';
+//db and authenticate user
+import connectDB from "./db/connect.js";
+
+//routers
+import authRouter from './routes/authRoutes.js'
 
 //middleware
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-app.get('/', (req, res) => {
-  res.send('welcome!')
-})
+app.use(express.json())
 
-const port = process.env.PORT || 5000
+app.get("/", (req, res) => {
+  res.send("welcome!");
+});
+
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL)
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
-      console.log(`Server is listening on port ${port}...`)
-    })
+      console.log(`Server is listening on port ${port}...`);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-start()
+start();
