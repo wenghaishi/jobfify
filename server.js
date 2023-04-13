ea939809533b3165fd4bd683ce98227be2beff8c
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-13 09:18:31 +0800

    created connect.js to connect mongodb

--- server.js
+++ server.js
@@ -5,17 +5,26 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import connectDB from './db/connect.js';
 
-app.get('/', (req, res) => {
-  throw new Error('error')
-  res.send('welcome!')
-})
-
+//middleware
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
+
+app.get('/', (req, res) => {
+  res.send('welcome!')
+})
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`)
 })
+
+const start = async () => {
+  try{
+    await connectDB(process.env.MONGO_URL)
+  } catch (error) {
+
+  }
+}
