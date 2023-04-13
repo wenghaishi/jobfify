d4d033a495648f40ff718b2d7d50099d21031b28
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-13 10:36:44 +0800

    set up express routes and auth controller

---
+++ routes/authRoutes.js
@@ -0,0 +1,8 @@
+import { Express } from "express";
+const router = express.Router();
+
+import { login, register, updateUser } from "../controllers/authController.js";
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/updateUser").post(updateUser);
