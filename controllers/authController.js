b4abf56cb9244a56542ab766c80eff4554a8bb4d
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-19 18:55:41 +0800

    create JWT token when user signs up

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,8 @@ const register = async (req, res, next) => {
   }
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user, token });
 };
 
 const login = async (req, res) => {
