521c258120427dd4f8e681aadcc343c25144a71b
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-19 17:09:23 +0800

    added checks for duplicate email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,21 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError, NotFoundError } from "../errors/index.js"
-
-
+import { BadRequestError, NotFoundError } from "../errors/index.js";
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
 
+  // Check for empty inputs
   if (!name || !password || !email) {
     throw new BadRequestError("Please provide all values");
   }
+
+  // Check for duplicate emails
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("email already in use");
+  }
+
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
