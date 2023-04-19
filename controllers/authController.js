007faad69e8126ecb09bade31d8090c87f5692c8
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-19 17:01:26 +0800

    refactor errors into errors folder

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,17 @@
 import User from "../models/User.js";
-import { StatusCodes } from 'http-status-codes'
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js"
+
+
 
 const register = async (req, res, next) => {
-    const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({ user });
+  const { name, email, password } = req.body;
+
+  if (!name || !password || !email) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
