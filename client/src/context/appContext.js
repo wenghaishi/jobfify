ec8bd5e3202018d3bdcf60d00b862f85fe150a79
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-20 17:26:30 +0800

    added register actions in actions.js

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,37 +1,40 @@
 import { useContext, useReducer } from "react";
-import React from 'react';
+import React from "react";
 import reducer from "./reducer";
-import { DISPLAY_ALERT, CLEAR_ALERT} from "./actions";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 const initialState = {
   isLoading: false,
   showAlert: false,
-  alertText: '',
-  alertType: ''
-}
+  alertText: "",
+  alertType: "",
+  user: null,
+  token: null,
+  userLocation: "",
+};
 
 const AppContext = React.createContext();
 
-const AppProvider = ({children}) => {
+const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const displayAlert = () => {
-    dispatch({type: DISPLAY_ALERT});
-  }
+    dispatch({ type: DISPLAY_ALERT });
+  };
 
   const clearAlert = () => {
-    dispatch({type: CLEAR_ALERT});
-  }
+    dispatch({ type: CLEAR_ALERT });
+  };
 
   return (
-    <AppContext.Provider value={{...state, displayAlert, clearAlert}}> 
+    <AppContext.Provider value={{ ...state, displayAlert, clearAlert }}>
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 
 const useAppContext = () => {
   return useContext(AppContext);
-}
+};
 
-export { AppProvider, initialState, useAppContext}
+export { AppProvider, initialState, useAppContext };
