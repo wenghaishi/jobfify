4820f5fcf0b428fb96dfb2e629e702c77e55b241
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-25 12:43:23 +0800

    created navbar, smallsidebar and bigsidebar

---
+++ client/src/components/BigSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from '../assets/wrappers/BigSidebar'
+
+const BigSidebar = () => {
+  return (
+    <Wrapper>
+      BigSidebar
+    </Wrapper>
+  )
+}
+
+export default BigSidebar
---
+++ client/src/components/SmallSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from '../assets/wrappers/SmallSidebar'
+
+const SmallSidebar = () => {
+  return (
+    <Wrapper>
+      Small sidebar
+    </Wrapper>
+  )
+}
+
+export default SmallSidebar
