378dfd88eb93960829a3341e8a5e79f308741a0d
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-25 13:25:19 +0800

    Navbar logic

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/Navbar'
+import React from "react";
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome, FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>navbar</h4>
-    </Wrapper>
-  )
-}
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onclick={() => console.log("clicked")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onclick={() => console.log("show/hide dropdown")}
+          >
+            <FaUserCircle />
+            John
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button type='button' className="dropdown-btn" onClick={() => console.log('logout')}>Logout</button>
+          </div>
 
-export default Navbar
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Navbar;
--- client/src/pages/Dashboard/SharedLayout.js
+++ client/src/pages/Dashboard/SharedLayout.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import Wrapper from "../../assets/wrappers/SharedLayout";
+import { Navbar, SmallSidebar, BigSidebar } from "../../components";
 
 const SharedLayout = () => {
   return (
     <Wrapper>
-      <nav>
-        <Link to="add-job">Add job</Link>
-        <Link to="all-jobs">All jobs</Link>
-      </nav>
-      <Outlet />
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
     </Wrapper>
   );
 };
