1105a4cc3b1afb9977a06c116f9d4c2bb0e2c073
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-05-02 00:10:53 +0800

    implemented create job method in jobs controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,16 @@
+import Job from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
+
+
 const createJob = async (req, res) => {
-  res.send('create jobs')
+  const { position, company} = req.body
+  if (!position || !company) {
+    throw new BadRequestError('Please provide all values')
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job });
 }
 
 const getAllJob = async (req, res) => {
