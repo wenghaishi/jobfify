5e4dfffeffd35014a3b317ea3df411dc200a0d1a
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-05-14 08:32:17 +0800

    implemented getAllJobs method

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,33 +1,35 @@
-import Job from '../models/Job.js';
-import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, NotFoundError } from '../errors/index.js';
-
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
 
 const createJob = async (req, res) => {
-  const { position, company} = req.body
+  const { position, company } = req.body;
   if (!position || !company) {
-    throw new BadRequestError('Please provide all values')
+    throw new BadRequestError("Please provide all values");
   }
   req.body.createdBy = req.user.userId;
-  const job = await Job.create(req.body)
+  const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
-}
+};
 
 const getAllJob = async (req, res) => {
-  res.send('getAllJobs')
-}
+  const jobs = await Job.find({ createdBy: req.user.userId });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+};
 
 const updateJob = async (req, res) => {
-  res.send('updateJobs')
-}
+  res.send("updateJobs");
+};
 
 const deleteJob = async (req, res) => {
-  res.send('deleteJob')
-}
+  res.send("deleteJob");
+};
 
 const showStats = async (req, res) => {
-  res.send('showStats')
-}
+  res.send("showStats");
+};
 
-export { createJob, getAllJob, updateJob, deleteJob, showStats }
-
+export { createJob, getAllJob, updateJob, deleteJob, showStats };
