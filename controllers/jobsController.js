06df0b86e7f8397e3c3fb3c9dd8a779f510359e2
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-05-20 16:26:44 +0800

    added logic for sorting and searching jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -29,8 +29,25 @@ const getAllJob = async (req, res) => {
     queryObject.jobType = jobType;
   }
 
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' };
+  }
+  // NO AWAIT
   let result = Job.find(queryObject);
 
+  // chain sort conditions
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position');
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position');
+  }
   const jobs = await result;
 
   res
