9541a5035bdf594b60b48ab3c4b0fc032579048b
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-05-21 18:12:49 +0800

    implemented frontend for searchContainer

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,11 +1,42 @@
-import React from 'react'
-
+import { FormRow, FormRowSelect } from ".";
+import { useAppContext } from "../context/appContext";
+import Wrapper from "../assets/wrappers/SearchContainer";
 const SearchContainer = () => {
-  return (
-    <h1>
-      Search component
-    </h1>
-  )
-}
+  const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext();
 
-export default SearchContainer
+  const handleSearch = (e) => {
+    if (isLoading) return;
+    handleChange({ name: e.target.name, value: e.target.value });
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <h4>search form</h4>
+        {/* search position */}
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          ></FormRow>
+          {/* rest of the inputs */}
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default SearchContainer;
