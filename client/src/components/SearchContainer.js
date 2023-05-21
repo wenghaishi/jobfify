0f21d9ef2cab2c04c7418c33d0ca81cabadcf5c1
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-05-21 18:16:23 +0800

    added filters for jobs search frontend

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,38 +1,73 @@
-import { FormRow, FormRowSelect } from ".";
-import { useAppContext } from "../context/appContext";
-import Wrapper from "../assets/wrappers/SearchContainer";
+import { FormRow, FormRowSelect } from '.';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/SearchContainer';
+
 const SearchContainer = () => {
   const {
     isLoading,
     search,
+    handleChange,
     searchStatus,
-    searchType,
-    sort,
-    sortOptions,
     statusOptions,
     jobTypeOptions,
-    handleChange,
+    searchType,
     clearFilters,
+    sort,
+    sortOptions,
   } = useAppContext();
 
   const handleSearch = (e) => {
     if (isLoading) return;
     handleChange({ name: e.target.name, value: e.target.value });
   };
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearFilters();
+  };
   return (
     <Wrapper>
-      <form className="form">
+      <form className='form'>
         <h4>search form</h4>
         {/* search position */}
-        <div className="form-center">
+        <div className='form-center'>
           <FormRow
-            type="text"
-            name="search"
+            type='text'
+            name='search'
             value={search}
             handleChange={handleSearch}
           ></FormRow>
-          {/* rest of the inputs */}
+          {/* search by status */}
+          <FormRowSelect
+            labelText='job status'
+            name='searchStatus'
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          ></FormRowSelect>
+          {/* search by type */}
+
+          <FormRowSelect
+            labelText='job type'
+            name='searchType'
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          ></FormRowSelect>
+          {/* sort */}
+
+          <FormRowSelect
+            name='sort'
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          ></FormRowSelect>
+          <button
+            className='btn btn-block btn-danger'
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            clear filters
+          </button>
         </div>
       </form>
     </Wrapper>
