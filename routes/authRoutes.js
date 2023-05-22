3ed6c834120f1f9aebc9de5142e64c70088a480f
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-05-22 09:48:21 +0800

    added request limiter for login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 const router = express.Router();
+import rateLimiter from 'express-rate-limit';
+
 import authenticateUser from "../middleware/auth.js";
 import { login, register, updateUser } from "../controllers/authController.js";
 
-router.route("/register").post(register);
-router.route("/login").post(login);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+
+router.route('/register').post(apiLimiter, register);
+router.route('/login').post(apiLimiter, login);
 router.route("/updateUser").patch(authenticateUser, updateUser);
 
 export default router;
